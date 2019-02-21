f0ed33ed4a2e3b339e4b5a879b9f23b5ccdb3948
ZhongZhaoCong <535377683@qq.com>
2019-02-21 23:36:34 +0800

    feat(button):增加button-icon

--- src/view/button.vue
+++ src/view/button.vue
@@ -1,9 +1,18 @@
 <template>
-   <button class="z-button">按钮</button>
+   <button class="z-button" :class="[`z-icon-${iconPosition}`]">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+   </button>
 </template>
 
 <script>
-  export default {}
+  export default {
+    props: [ 'icon', 'iconPosition' ]
+  }
 </script>
 
 <style lang="scss">
@@ -15,6 +24,14 @@
     border-radius: var(--border-radius);
     border: 1px solid var(--border-color);
     background: var(--button-bg);
+
+    /* 内容都居中 */
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+
+    /* 弄掉行内元素默认基线对齐 */
+    vertical-align: middle;
     &:hover {
       border-color: var(--border-color-hover);
     }
@@ -24,6 +41,21 @@
     /* 后面再优化focus样式： */
     &:focus {
       outline: none;
+    }
+
+    >.icon {
+      order: 1;
+    }
+    >.content {
+      order: 2;
+    }
+    &.z-icon-right {
+      >.icon {
+        order: 2;
+      }
+      >.content {
+        order: 1;
+      }
     }
   }
 </style>
