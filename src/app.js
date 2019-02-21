b0e6c32422462beef07a95a5696d9a3a14478860
ZhongZhaoCong <535377683@qq.com>
2019-02-21 22:18:10 +0800

    feat(project):加parcel button改为单文件组件

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import Button from './view/button'
+
+Vue.component('z-button', Button)
+
+new Vue({
+  el: '#app'
+})
---
+++ src/view/button.vue
@@ -0,0 +1,29 @@
+<template>
+   <button class="z-button">按钮</button>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="scss">
+  .z-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    /* 写死width不好： */
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+    &:hover {
+      border-color: var(--border-color-hover);
+    }
+    &:active {
+      background: var(--button-active-bg);
+    }
+    /* 后面再优化focus样式： */
+    &:focus {
+      outline: none;
+    }
+  }
+</style>
