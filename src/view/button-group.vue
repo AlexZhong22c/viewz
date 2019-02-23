c20483a8ab33c9cace468efbd0a875660180c1a3
ZhongZhaoCong <535377683@qq.com>
2019-02-23 08:26:26 +0800

    feat(button):group检测子元素是不是button
    因为影响的只是样式，不会导致程序错误，所以只是提示就行了

--- src/view/button-group.vue
+++ src/view/button-group.vue
@@ -5,7 +5,16 @@
 </template>
 
 <script>
-  export default {}
+  export default {
+    mounted() {
+      for (const node of this.$el.children) {
+        const name = node.nodeName.toLowerCase()
+        if (name !== 'button') {
+          console.warn(`z-button-group 的子元素应该全是 z-button，但是你写的是 ${name}`)
+        }
+      }
+    }
+  }
 </script>
 
 <style lang="scss">
