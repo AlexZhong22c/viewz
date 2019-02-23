980941aa8e45a1ed6e6ca40341915b4df6630273
ZhongZhaoCong <535377683@qq.com>
2019-02-23 08:14:18 +0800

    fea(button-group):完成创建

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Button from './view/button'
+import ButtonGroup from './view/button-group'
 import Icon from './view/icon'
 
 Vue.component('z-button', Button)
+Vue.component('z-button-group', ButtonGroup)
 Vue.component('z-icon', Icon)
 
 new Vue({
---
+++ src/view/button-group.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="z-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="scss">
+  .z-button-group {
+    display: inline-flex;
+    vertical-align: middle;
+    > .z-button {
+      border-radius: 0;
+      &:first-child {
+        border-top-left-radius: var(--border-radius);
+        border-bottom-left-radius: var(--border-radius);
+      }
+      &:last-child {
+        border-top-right-radius: var(--border-radius);
+        border-bottom-right-radius: var(--border-radius);
+      }
+
+      // 解决边框问题：
+      // 在hover的时候才让它走到上层：
+      margin-right: -1px;
+      &:hover {
+        position: relative;
+        z-index: 1;
+      }
+    }
+  }
+</style>
