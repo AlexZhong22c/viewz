7fc4d641dd5482ce345d2ca1fa7207800c46b254
ZhongZhaoCong <535377683@qq.com>
2019-02-23 11:45:21 +0800

    test(button): 测试icon loading iconPosition属性

--- src/app.js
+++ src/app.js
@@ -10,3 +10,84 @@ Vue.component('z-icon', Icon)
 new Vue({
   el: '#app'
 })
+
+/**
+ * 单元测试
+ */
+import chai from 'chai'
+const expect = chai.expect
+// console.log(Button)
+// Button是一个对象，ButtonConstructor是一个函数：
+const ButtonConstructor = Vue.extend(Button)
+/**
+ * test icon prop:
+ */
+// {
+//   const vm = new ButtonConstructor({
+//     propsData: {
+//       icon: 'settings'
+//     }
+//   })
+//   vm.$mount('#test-assert')
+//   const useElement = vm.$el.querySelector('use')
+//   const href = useElement.getAttribute('xlink:href')
+//   expect(href).to.eq('#i-settings')
+// }
+
+/**
+ * test icon prop + loading prop:
+ */
+// {
+//   const vm = new ButtonConstructor({
+//     propsData: {
+//       icon: 'settings',
+//       loading: true
+//     }
+//   })
+//   vm.$mount('#test-assert')
+//   const useElement = vm.$el.querySelector('use')
+//   const href = useElement.getAttribute('xlink:href')
+//   expect(href).to.eq('#i-loading')
+// }
+
+/**
+ * @file 优化测试代码后：
+ * 1. document.body.appendChild(iDiv)
+ * 2. vm.$el.remove();vm.$destroy()
+ */
+/**
+ * test iconPosition prop(flex):
+ */
+{
+  const iDiv = document.createElement('div')
+  document.body.appendChild(iDiv)
+  const vm = new ButtonConstructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  vm.$mount(iDiv)
+  const svgElement = vm.$el.querySelector('svg')
+  const { order } = window.getComputedStyle(svgElement)
+  expect(order).to.eq('1')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+{
+  const iDiv = document.createElement('div')
+  document.body.appendChild(iDiv)
+  const vm = new ButtonConstructor({
+    propsData: {
+      icon: 'settings',
+      iconPosition: 'right'
+    }
+  })
+  vm.$mount(iDiv)
+  const svgElement = vm.$el.querySelector('svg')
+  const { order } = window.getComputedStyle(svgElement)
+  expect(order).to.eq('2')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
