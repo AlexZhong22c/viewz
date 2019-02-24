23b3792940e614e11bad62e69701380b64542986
ZhongZhaoCong <535377683@qq.com>
2019-02-24 21:38:10 +0800

    feat(input): input样式和基本状态

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Button from './view/button'
 import ButtonGroup from './view/button-group'
 import Icon from './view/icon'
+import Input from './view/input'
 
 Vue.component('z-button', Button)
 Vue.component('z-button-group', ButtonGroup)
 Vue.component('z-icon', Icon)
+Vue.component('z-input', Input)
 
 new Vue({
   el: '#app'
