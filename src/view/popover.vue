1eca977604b9af2b90367d618d18772fa919b1c0
ZhongZhaoCong <535377683@qq.com>
2019-03-08 23:07:06 +0800

    fix(popover): 容器有overflow:hidden裁掉弹窗

--- src/view/popover.vue
+++ src/view/popover.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="z-popover"
     @click.stop="xxxxxx">
-    <slot></slot>
-    <div class="z-popover__content-wrapper" @click.stop
+    <span ref="triggerWrapper"><slot></slot></span>
+    <div class="z-popover__content-wrapper"
+      ref="contentWrapper"
       v-if="visible">
       <slot name="content"></slot>
     </div>
@@ -20,6 +21,14 @@ export default {
       this.visible = !this.visible
       if (this.visible) {
         this.$nextTick(() => {
+          // 这句能否放在设置left top那两句之后??
+          document.body.appendChild(this.$refs.contentWrapper)
+
+          const { top, left } = this.$refs.triggerWrapper.getBoundingClientRect()
+          // TODO: window.scrollX要兼容不同浏览器
+          this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+          this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+
           // 点击body的时候能隐藏popover:
           const eventHandler = () => {
             this.visible = false
@@ -39,12 +48,12 @@ export default {
   display: inline-block;
   vertical-align: top;
   position: relative;
-  .z-popover__content-wrapper {
-    position: absolute;
-    bottom: 100%;
-    left: 0;
-    border: 1px solid red;
-    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-  }
+}
+// 该元素被添加在body元素末端：
+.z-popover__content-wrapper {
+  position: absolute;
+  transform: translateY(-100%);
+  border: 1px solid red;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
 }
 </style>
