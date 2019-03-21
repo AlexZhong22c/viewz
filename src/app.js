5c56d30a2c7736420ae2cae7b2a4f70a4808ed12
ZhongZhaoCong <535377683@qq.com>
2019-03-21 18:23:21 +0800

    style(app): 利用编辑器整理大段代码

--- src/app.js
+++ src/app.js
@@ -1,46 +1,47 @@
 import Vue from 'vue'
+
 import Button from './view/button'
 import ButtonGroup from './view/button-group'
-import Icon from './view/icon'
-import Input from './view/input'
-import Row from './view/row'
 import Col from './view/col'
-import Layout from './view/layout'
-import Header from './view/layout/header'
-import Content from './view/layout/content'
-import Footer from './view/layout/footer'
-import Sider from './view/layout/sider'
-// import Toast from './view/toast'
-import plugin from './view/plugin'
-import Tabs from './view/tabs'
-import TabsHead from './view/tabs/tabs-head'
-import TabsBody from './view/tabs/tabs-body'
-import TabsItem from './view/tabs/tabs-item'
-import TabsPane from './view/tabs/tabs-pane'
-import Popover from './view/popover'
 import Collapse from './view/collapse'
 import CollapseItem from './view/collapse-item'
+import Content from './view/layout/content'
+import Footer from './view/layout/footer'
+import Header from './view/layout/header'
+import Icon from './view/icon'
+import Input from './view/input'
+import Layout from './view/layout'
+import Popover from './view/popover'
+import Row from './view/row'
+import Sider from './view/layout/sider'
+import Tabs from './view/tabs'
+import TabsBody from './view/tabs/tabs-body'
+import TabsHead from './view/tabs/tabs-head'
+import TabsItem from './view/tabs/tabs-item'
+import TabsPane from './view/tabs/tabs-pane'
 
 Vue.component('z-button', Button)
 Vue.component('z-button-group', ButtonGroup)
-Vue.component('z-icon', Icon)
-Vue.component('z-input', Input)
-Vue.component('z-row', Row)
 Vue.component('z-col', Col)
-Vue.component('z-layout', Layout)
-Vue.component('z-header', Header)
-Vue.component('z-content', Content)
-Vue.component('z-footer', Footer)
-Vue.component('z-sider', Sider)
-Vue.component('z-tabs', Tabs)
-Vue.component('z-tabs-head', TabsHead)
-Vue.component('z-tabs-body', TabsBody)
-Vue.component('z-tabs-item', TabsItem)
-Vue.component('z-tabs-pane', TabsPane)
-Vue.component('z-popover', Popover)
 Vue.component('z-collapse', Collapse)
 Vue.component('z-collapse-item', CollapseItem)
+Vue.component('z-content', Content)
+Vue.component('z-footer', Footer)
+Vue.component('z-header', Header)
+Vue.component('z-icon', Icon)
+Vue.component('z-input', Input)
+Vue.component('z-layout', Layout)
+Vue.component('z-popover', Popover)
+Vue.component('z-row', Row)
+Vue.component('z-sider', Sider)
+Vue.component('z-tabs', Tabs)
+Vue.component('z-tabs-body', TabsBody)
+Vue.component('z-tabs-head', TabsHead)
+Vue.component('z-tabs-item', TabsItem)
+Vue.component('z-tabs-pane', TabsPane)
 
+import plugin from './view/plugin'
+// import Toast from './view/toast'
 // Vue.component('z-toast', Toast)
 // 通过js实例方法创建组件实例：
 Vue.use(plugin)
