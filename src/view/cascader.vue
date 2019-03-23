4adaee8bb3e765fa19ebad9b7295875bdb5aa41c
ZhongZhaoCong <535377683@qq.com>
2019-03-23 19:44:39 +0800

    feat(cascader): 完成基本样式

--- src/view/cascader.vue
+++ src/view/cascader.vue
@@ -2,6 +2,8 @@
   <div class="z-cascader">
     <div class="z-cascader__trigger"
       @click="popoverVisible = !popoverVisible">
+      <!-- 不因为突然有字在里面而高度变化 -->
+      {{ result || '&nbsp;' }}
     </div>
     <div class="z-cascader__popover-wrapper"
       v-if="popoverVisible">
@@ -39,6 +41,11 @@ export default {
       popoverVisible: false
     }
   },
+  computed: {
+    result() {
+      return this.selected.map(item => item.name).join('/')
+    }
+  },
   methods: {
     onUpdateSelected (selected) {
       this.$emit('update:selected', selected)
@@ -54,14 +61,21 @@ export default {
 .z-cascader {
   position: relative;
   .z-cascader__trigger {
-    border: 1px solid black;
-    height: 32px;
-    width: 100px;
+    height: $z-button-height;
+    min-width: 10em;
+    padding: 0 1em;
+    border: 1px solid $z-border-gray1;
+    border-radius: $z-border-radius;
+
+    display: inline-flex;
+    align-items: center;
   }
   .z-cascader__popover-wrapper {
     position: absolute;
     top: 100%;
     left: 0;
+
+    margin-top: 8px;
 
     background: white;
 
