334d30b4a471d7e30b775980727238c9079a4657
ZhongZhaoCong <535377683@qq.com>
2019-04-05 12:04:23 +0800

    docs(input): 初步编写文档

---
+++ docs/.vuepress/components/input-demo-1.vue
@@ -0,0 +1,47 @@
+<template>
+  <div style="padding-top: 16px;">
+    <h2>简单用法</h2>
+    <p>
+      <strong>预览</strong>
+    </p>
+    <z-input></z-input>
+    <z-input value="中文"></z-input>
+    <z-input value="中文"
+      disabled></z-input>
+    <z-input value="中文"
+      readonly></z-input>
+
+    <p>
+      <strong>代码</strong>
+    </p>
+    <pre><code>{{content}}</code></pre>
+  </div>
+</template>
+<script>
+import ZInput from '../../../src/view/input'
+
+export default {
+  components: { ZInput },
+  data() {
+    return {
+      selected: '1',
+      content: `
+        data() {
+          return {
+            selected: '1',
+          }
+        }
+
+        <z-input></z-input>
+        <z-input value="中文"></z-input>
+        <z-input value="中文"
+          disabled></z-input>
+        <z-input value="中文"
+          readonly></z-input>
+      `
+        .replace(/ {8}/g, '')
+        .trim()
+    }
+  }
+}
+</script>
---
+++ docs/.vuepress/components/input-demo-2.vue
@@ -0,0 +1,39 @@
+<template>
+  <div style="padding-top: 16px;">
+    <h2>双向绑定</h2>
+    <p>
+      <strong>预览</strong>
+    </p>
+    <z-input v-model="value"></z-input>
+    <div>value: {{value}}</div>
+
+    <p>
+      <strong>代码</strong>
+    </p>
+    <pre><code>{{content}}</code></pre>
+  </div>
+</template>
+<script>
+import ZInput from '../../../src/view/input'
+
+export default {
+  components: { ZInput },
+  data() {
+    return {
+      value: '1',
+      content: `
+          data: {
+            return {
+              value: '1'
+            }
+          }
+
+          <z-input v-model="value"></z-input>
+          <div>value: {{value}}</div>
+      `
+        .replace(/ {8}/g, '')
+        .trim()
+    }
+  }
+}
+</script>
