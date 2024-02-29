e77db7f1d5a32cdc33d191b470f0c9e61c0e5e1a
22c <edgar52@163.com>
2024-02-29 21:39:03 +0800

    feat(project): 增加隐藏路由

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,19 @@
 const path = require('path')
 
 module.exports = {
+  head: [ ['style', {}, `
+/*  “隐藏路由”  */
+.nav-links .nav-item:last-child { display:none;} 
+`]],
   title: 'VIEWZ UI',
   base: '/viewz/',
   description: '一个好用的UI库',
   themeConfig: {
     nav: [
       {text: '主页', link: '/'},
+      {text: '主页2', link: '/xx'},
+      {text: '主页3', link: '/rr'},
+      { text: '隐藏路由', link: '/hide/' },
       // {text: '文档', link: '/guide/'},
       // {text: '交流', link: 'https://google.com'},
     ],
