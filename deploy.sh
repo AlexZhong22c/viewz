1fceeaeb480620a186f4e932681cac407260610a
22c <edgar52@163.com>
2024-02-29 23:17:42 +0800

    fix(project): 部署vuepress的临时分支

--- deploy.sh
+++ deploy.sh
@@ -17,10 +17,10 @@ sudo git add -A
 sudo git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:AlexZhong22c/viewz.git master:gh-pages
-git push -f git@github.com:AlexZhong22c/viewz.git master:gh-pages
+# git push -f git@github.com:AlexZhong22c/viewz.git main:gh-pages
+git push -f git@github.com:AlexZhong22c/viewz.git main:gh-pages
 
 cd -
