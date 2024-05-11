b686ffcab4bd739937cf03161682f649cf01e0a6
22c <edgar52@163.com>
2024-05-11 21:24:24 +0800

    fix(global): 去掉sudo

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
+# 【但是此方案 平台能力限制只能使用最新的node】或者使用这个替代方案：https://vuepress.github.io/zh/guide/deployment.html#vercel
 
 # 确保脚本抛出遇到的错误
 set -e
 
 # 生成静态文件
-sudo npm run docs:build
+npm run docs:build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -12,9 +13,9 @@ cd docs/.vuepress/dist
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-sudo git init
-sudo git add -A
-sudo git commit -m 'deploy'
+git init
+git add -A
+git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
